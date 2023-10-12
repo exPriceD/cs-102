a8e5da74351ab211f5b1713b9f8d025681bdd054
exPriceD <lolerpro4@gmail.com>
2023-10-12 18:49:09 +0300

    Реализована функция is_prime(n)

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -12,8 +12,10 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 def gcd(a: int, b: int) -> int:
