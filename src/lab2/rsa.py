cd56819c50df2fa3bd83fea8a65c70310a85103c
exPriceD <lolerpro4@gmail.com>
2023-10-17 20:13:03 +0300

    Ошибочка вышла

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -88,7 +88,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
 
 if __name__ == "__main__":
     print("RSA Encrypter/ Decrypter")
-    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    p = int(input("Enter a prime number (17, 19, 23, 41, etc): "))
     q = int(input("Enter another prime number (Not one you entered above): "))
     print("Generating your public/private keypairs now . . .")
     public, private = generate_keypair(p, q)
