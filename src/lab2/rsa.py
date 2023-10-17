5c02431fa35ea0a631d8fafc5bc144ae11ae6098
exPriceD <lolerpro4@gmail.com>
2023-10-17 19:27:28 +0300

    RSA

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -64,7 +64,7 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
-    return ((e, n), (d, n))
+    return (e, n), (d, n)
 
 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
